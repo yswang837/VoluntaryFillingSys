77cb589d44826300830acc1bd342af27de4f7c4d
yswang837 <yswang837@gmail.com>
2022-08-13 08:35:05 +0800

    完成通过导入github.com/yswang837/snowflake包来实现分布式id生成

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/bwmarrin/snowflake v0.3.0
 	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/yswang837/email v0.0.1
+	github.com/yswang837/snowflake v0.0.1
 	gopkg.in/ini.v1 v1.67.0
 )
--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/VoluntaryFillingSys/pkg/snowflake"
 	"github.com/VoluntaryFillingSys/utils"
 	"github.com/yswang837/email"
+	"github.com/yswang837/snowflake"
 )
 
 func main() {
 	if err := snowflake.Init(utils.StartTime, utils.MachinedId); err != nil {
 		return
 	}
-	fmt.Println(snowflake.Node.Generate())
+	fmt.Println(snowflake.GenID("AAB"))
 	code, err := email.SendEmail("1714113169@qq.com")
 	if err != nil {
 		panic(err)
