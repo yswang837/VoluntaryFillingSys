bfe25da16a0fb3cdb7b09f880ea06634c28acd30
yswang837 <yswang837@gmail.com>
2022-08-13 08:00:53 +0800

    完成通过导入github.com/yswang837/email包来实现发送邮件

--- go.mod
+++ go.mod
@@ -4,8 +4,7 @@ go 1.16
 
 require (
 	github.com/bwmarrin/snowflake v0.3.0
-	github.com/jinzhu/gorm v1.9.16
-	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
-	gopkg.in/ini.v1 v1.66.6
+	github.com/yswang837/email v0.0.1
+	gopkg.in/ini.v1 v1.67.0
 )
--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 	"github.com/VoluntaryFillingSys/pkg/snowflake"
 	"github.com/VoluntaryFillingSys/utils"
-	"github.com/jordan-wright/email"
-	"net/smtp"
+	"github.com/yswang837/email"
 )
 
 func main() {
@@ -13,16 +12,11 @@ func main() {
 		return
 	}
 	fmt.Println(snowflake.Node.Generate())
-	e := email.NewEmail()
-	e.From = "wys <1714113169@qq.com>"
-	e.To = []string{"1714113169@qq.com"}
-	e.Bcc = []string{"1714113169@qq.com"}
-	e.Cc = []string{"1714113169@qq.com"}
-	e.Text = []byte("本文邮件内容")
-	e.HTML = []byte("<h1>html 邮件内容!</h1>")
-	if err := e.Send("smtp.qq.com:587", smtp.PlainAuth("", "1714113169@qq.com", "bxvjwuyyeoqfdced", "smtp.qq.com")); err != nil {
-		fmt.Println(err)
+	code, err := email.SendEmail("1714113169@qq.com")
+	if err != nil {
+		panic(err)
 	}
+	fmt.Println("code", code)
 
 }
 
